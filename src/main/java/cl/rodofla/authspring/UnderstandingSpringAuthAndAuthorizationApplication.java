00b0c2662e93b80f08fa119232088b9d2111a17b
Rodrigo Flores <rodfloresp@gmail.com>
2025-01-28 23:28:10 -0300

    initialize Spring Boot application with basic configuration and structure

---
+++ src/main/java/cl/rodofla/authspring/UnderstandingSpringAuthAndAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package cl.rodofla.authspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandingSpringAuthAndAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderstandingSpringAuthAndAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/rodofla/authspring/UnderstandingSpringAuthAndAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.rodofla.authspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandingSpringAuthAndAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
